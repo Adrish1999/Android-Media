a746e18cf618cb1a59087dbcbce94a8379220d16
Adrish Bose <adrishbose198@gmail.com>
2020-11-22 18:31:10 +0530

    Media integrated

--- app/src/main/java/com/adrish/playmedia/MainActivity.java
+++ app/src/main/java/com/adrish/playmedia/MainActivity.java
@@ -2,13 +2,29 @@ package com.adrish.playmedia;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private MediaPlayer mediaPlayer;
+    private Button playButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mediaPlayer = new MediaPlayer();
+        mediaPlayer = MediaPlayer.create(MainActivity.this,R.raw.kalimba);
+
+        playButton = findViewById(R.id.Play_Button);
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
