a6b2f4a4dfd39d25d3b5cab943872ac5351910bb
Adrish Bose <adrishbose198@gmail.com>
2020-11-26 12:08:19 +0530

    Seekbar added

--- app/src/main/java/com/adrish/playmedia/MainActivity.java
+++ app/src/main/java/com/adrish/playmedia/MainActivity.java
@@ -70,7 +70,10 @@ public class MainActivity extends AppCompatActivity {
         seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-
+                    if(fromUser)
+                    {
+                        mediaPlayer.seekTo(progress);
+                    }
             }
 
             @Override
